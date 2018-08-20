7a3f2b7406cdb29bde92f13d135b32288dee2be1
alubowsky <zevelmail13@gmail.com>
2018-08-19 20:09:39 -0500

    mixed content take 2

--- maps.js
+++ maps.js
@@ -73,7 +73,7 @@
     google.maps.event.addListener(drawingManager, 'rectanglecomplete', function (drawing) {
         clearMap();
         rectangle = drawing;
-        $.getJSON("http://api.geonames.org/wikipediaBoundingBox?",
+        $.getJSON("https://api.geonames.org/wikipediaBoundingBox?",
             {
                 north: drawing.getBounds().f.b,
                 south: drawing.getBounds().f.f,
@@ -119,7 +119,7 @@
 
     $('#go').click(function () {
         clearMap();
-        $.getJSON("http://api.geonames.org/wikipediaSearch?callback=?",
+        $.getJSON("https://api.geonames.org/wikipediaSearch?callback=?",
             {
                 q: search.val(),
                 maxRows: numResultsInput.val(),
